9cbf008123684a54a3c1191245f75beeab85b37a
Pedro Marins <contato@pedromarins.com>
2016-03-08 01:04:49 -0300

    creating service to carry repo data

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,18 +1,23 @@
 var repoControllers = angular.module('repoControllers', []);
 
-repoControllers.controller('RepoListCtrl', ['$scope', '$http',
-	function ($scope, $http) {
+repoControllers.controller('RepoListCtrl', ['$rootScope', '$scope', '$http', 'currentRepo',
+	function ($rootScope, $scope, $http, currentRepo) {
 
 
 		$http.get('https://api.github.com/orgs/netflix/repos?per_page=1000').success(function(data) {
 			console.log(data.length);
 			$scope.repos = data;
 		});
+
+		$scope.carryData = function (data) {
+			console.log(data);
+			console.log(data.forks);
+			currentRepo.set(data);
+		}
 	}]);
 
-repoControllers.controller('CommitListCtrl', ['$scope', '$http', '$routeParams',
-	
-	function($scope, $http, $routeParams) {
+repoControllers.controller('CommitListCtrl', ['$rootScope', '$scope', '$http', 'currentRepo', '$routeParams',
+	function($rootScope, $scope, $http, currentRepo, $routeParams) {
     	$scope.repoName = $routeParams.repoName;
     	$scope.currentPage = 1;
     	$scope.totalPages = 1;
@@ -28,7 +33,37 @@ repoControllers.controller('CommitListCtrl', ['$scope', '$http', '$routeParams',
 				console.log($scope.commits);
 				$scope.currentPage++;
 			});
+			console.log($scope.currentPage);
+		};
 
-			$scope.currentPage++;
-		}
+		$scope.repo = null;
+		
+		(function () {
+			$scope.$watch(
+				function () { 
+					return currentRepo.get(); 
+				},
+				function (newValue, oldValue) {
+	        		if (newValue != null) {
+	            		$scope.repo = newValue;
+	            		console.log($scope.repo.forks);
+	        		}
+	    		}, true
+	    	);
+	    })();
 	}]);
+
+repoControllers.service("currentRepo", function () {
+
+	var repo = {};
+
+	return {
+	    get: function () {
+	        return repo;
+	    },
+	    set: function (value) {
+	        repo = value;
+	    }
+	};
+
+});

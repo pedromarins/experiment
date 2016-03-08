e8acfd4965dd1bce9a2ed90ceb81cfbffc671927
Pedro Marins <contato@pedromarins.com>
2016-03-08 01:39:11 -0300

    using commits number to improve loadMore commits function
    still have to hide button

--- app/js/controller.js
+++ app/js/controller.js
@@ -4,14 +4,12 @@ repoControllers.controller('RepoListCtrl', ['$rootScope', '$scope', '$http', 'cu
 	function ($rootScope, $scope, $http, currentRepo) {
 
 
-		$http.get('https://api.github.com/orgs/netflix/repos?per_page=1000').success(function(data) {
-			console.log(data.length);
+		$http.get('https://api.github.com/orgs/Netflix/repos?per_page=1000').success(function(data) {
+			// console.log(data.length);
 			$scope.repos = data;
 		});
 
 		$scope.carryData = function (data) {
-			console.log(data);
-			console.log(data.forks);
 			currentRepo.set(data);
 		}
 	}]);
@@ -20,24 +18,25 @@ repoControllers.controller('CommitListCtrl', ['$rootScope', '$scope', '$http', '
 	function($rootScope, $scope, $http, currentRepo, $routeParams) {
     	$scope.repoName = $routeParams.repoName;
     	$scope.currentPage = 1;
-    	$scope.totalPages = 1;
+    	$scope.totalPages = null;
+    	$scope.repo = null;
 
-		$http.get('https://api.github.com/repos/netflix/' + $scope.repoName + '/commits?page=' + $scope.currentPage + '&per_page=20').success(function(data) {
+		$http.get('https://api.github.com/repos/Netflix/' + $scope.repoName + '/commits?page=' + $scope.currentPage + '&per_page=20').success(function(data) {
 			$scope.commits = data;
 			$scope.currentPage++;
 		});
 
+		// load more 20 commits
 		$scope.loadMore = function () {
-			$http.get('https://api.github.com/repos/netflix/' + $scope.repoName + '/commits?page=' + $scope.currentPage + '&per_page=20').success(function(data) {
-				$scope.commits.push.apply($scope.commits, data);
-				console.log($scope.commits);
-				$scope.currentPage++;
-			});
-			console.log($scope.currentPage);
+			if ($scope.currentPage < $scope.totalPages) {
+				$http.get('https://api.github.com/repos/Netflix/' + $scope.repoName + '/commits?page=' + $scope.currentPage + '&per_page=20').success(function(data) {
+					$scope.commits.push.apply($scope.commits, data);
+					$scope.currentPage++;
+				});
+			};
 		};
 
-		$scope.repo = null;
-		
+		// repo data provided by currentRepo service
 		(function () {
 			$scope.$watch(
 				function () { 
@@ -46,7 +45,6 @@ repoControllers.controller('CommitListCtrl', ['$rootScope', '$scope', '$http', '
 				function (newValue, oldValue) {
 	        		if (newValue != null) {
 	            		$scope.repo = newValue;
-	            		console.log($scope.repo.forks);
 	        		}
 	    		}, true
 	    	);
@@ -56,6 +54,14 @@ repoControllers.controller('CommitListCtrl', ['$rootScope', '$scope', '$http', '
 	    (function () {
 		    $http.get('https://api.github.com/repos/Netflix/' + $scope.repoName + '/stats/contributors').success(function(data) {
 				$scope.contributors = data;
+				
+				var commitsNumber = 0;
+				angular.forEach($scope.contributors, function(value, key){
+					commitsNumber = commitsNumber + value.total;
+				});
+				console.log(commitsNumber);
+				$scope.totalPages = commitsNumber / 20;
+			});
 		})();
 		
 	}]);
